1815300c05ddb5483ddb66febdc3c546a1293831
Steve010300 <stleonard978@gmail.com>
2025-08-21 00:14:37 -0400

    Add Secure-the-Vault

---
+++ index.js
@@ -0,0 +1,26 @@
+let code1;
+
+code1 = 7 + 3;
+
+const code2 = (84 / 2) - 2;
+ 
+const code3 = (13 % 12) + 38;
+
+let message;
+
+message = "The vault has been secured. The combination is: 104039"
+
+let codeA;
+
+codeA = code1 + code2 + code3;
+
+console.log( codeA = code1 + "-" + code2 + "-" + code3 );
+
+
+let codeB; 
+
+codeB = code1 + code2 + code3;
+
+console.log( `${code1}-${code2}-${code3}` );
+
+console.log ( "message:", message, "codeA", codeA, "codeB", codeB);
